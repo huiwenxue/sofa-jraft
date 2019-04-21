29b8778fc04bafee7ef058e60d0515fad924944f
xuehuiwen <xue95326@163.com>
2019-04-21 12:22:08 +0800

    新增测试类

---
+++ jraft-core/src/test/java/com/alipay/sofa/jraft/mytest/MyTest.java
@@ -0,0 +1,13 @@
+package com.alipay.sofa.jraft.mytest;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyTest.class);
+    @Test
+    public void test01(){
+        LOGGER.error("this is my first test");
+    }
+}
